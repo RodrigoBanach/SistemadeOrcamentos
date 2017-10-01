5739d67a5d2185ce0ad91e3dcfabb5fa828b9e6a
rodri <rodrigo.banach@yahoo.com>
2017-09-30 22:13:11 -0300

    Designer

---
+++ app/src/main/java/com/sos/sistemadeorcamentos/TelaAdmin.java
@@ -0,0 +1,15 @@
+package com.sos.sistemadeorcamentos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TelaAdmin extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_admin);
+    }
+}
