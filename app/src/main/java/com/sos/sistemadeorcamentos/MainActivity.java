4825e15e8a0675d7a90b3116c2c34863d3da02be
jhonattan90 <jhonattan90>
2017-11-20 10:31:12 -0200

    facebook login

--- app/src/main/java/com/sos/sistemadeorcamentos/MainActivity.java
+++ app/src/main/java/com/sos/sistemadeorcamentos/MainActivity.java
@@ -5,24 +5,127 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-public class MainActivity extends AppCompatActivity {
-    Button button1;
+
+public class MainActivity extends Activity {
+     private CallbackManager callbackManager;
+     private LoginButton loginButton;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
-        button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.loginButton);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View v) {
-                Intent abreCadastro = new Intent(MainActivity.this, CadastroClientes.class);
-                startActivity(abreCadastro);
+            public void onSuccess(LoginResult loginResult) {
+                Log.i("ID_FB",loginResult.getAccessToken().getUserId());
+                redirect(loginResult.getAccessToken().getUserId());
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
 
             }
         });
     }
+
+
+
+    @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+
+        private void redirect(String ID){
+        Intent intent = new Intent(MainActivity.this,CadastroClientes.class);
+        intent.putExtra("FB_ID",ID);
+        startActivity(intent);
+    }
+
+    }
+
+/*
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        }
+        catch (PackageManager.NameNotFoundException e) {
+
+        }
+        catch (NoSuchAlgorithmException e) {
+
+        }
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i("ID_FB",loginResult.getAccessToken().getUserId());
+                redirect(loginResult.getAccessToken().getUserId());
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if(accessToken != null){
+            redirect(accessToken.getUserId());
+        }
+    }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+
+
+    private void redirect(String ID){
+        Intent intent = new Intent(MainActivity.this,CadastroClientes.class);
+        intent.putExtra("FB_ID",ID);
+        startActivity(intent);
+    }
 }
+*/
